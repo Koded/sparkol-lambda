08a680b112b49ba3161d1647253bbc7d6205d69c
Jon <jon@kode.uk.com>
2019-05-22 12:18:19 +0100

    Updating to use SSM

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,19 +1,48 @@
+const _ = require('lodash')
 const { promisify } = require('util')
 const awscred = require('awscred')
+const { REGION, STAGE } = process.env
+const AWS = require('aws-sdk')
+AWS.config.region = REGION
+const SSM = new AWS.SSM()
 
 let initialized = false
+
+const getParameters = async (keys) => {
+  const prefix = `/workshop-jon/${STAGE}/`
+  const req = {
+    Names: keys.map(key => `${prefix}${key}`)
+  }
+  const resp = await SSM.getParameters(req).promise()
+  return _.reduce(resp.Parameters, function(obj, param) {
+    obj[param.Name.substr(prefix.length)] = param.Value
+    return obj
+   }, {})
+}
 
 const init = async () => {
   if (initialized) {
     return
   }
 
-  process.env.TEST_ROOT = "https://3es789lt7h.execute-api.us-east-1.amazonaws.com/dev"
-  process.env.restaurants_api      = "https://3es789lt7h.execute-api.us-east-1.amazonaws.com/dev/restaurants"
-  process.env.restaurants_table    = "restaurants-jon"
-  process.env.AWS_REGION           = "us-east-1"
-  process.env.cognito_user_pool_id = "us-east-1_3T4PzC39G"
-  process.env.cognito_client_id    = "5bbrva6nellhslfvn1ei4s6o4i"
+
+  const params = await getParameters([
+    'table_name',
+    'cognito_user_pool_id',
+    'cognito_web_client_id',
+    'cognito_server_client_id',
+    'url'
+  ])
+
+  console.log('SSM params loaded')
+
+  process.env.TEST_ROOT                = params.url
+  process.env.restaurants_api          = `${params.url}/restaurants`
+  process.env.restaurants_table        = params.table_name
+  process.env.AWS_REGION               = REGION
+  process.env.cognito_user_pool_id     = params.cognito_user_pool_id
+  process.env.cognito_client_id        = params.cognito_web_client_id
+  process.env.cognito_server_client_id = params.cognito_server_client_id
 
   const { credentials } = await promisify(awscred.load)()
 
